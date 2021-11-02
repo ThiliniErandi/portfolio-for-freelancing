7c8e1c780ef3e17120527b8705eedc66ade51f58
ThiliniErandi <thilini.e97@gmail.com>
2021-11-02 17:54:10 +0530

    Added Assets Folder

---
+++ assets/js/main.js
@@ -0,0 +1,137 @@
+(function ($) {
+    "use strict";
+
+/*********** Navbar Area **********/
+
+     /*** Navbar Sticky ***/ 
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= 1) {
+            $(".navbar").addClass("bg-primari");
+        } else {
+            $(".navbar").removeClass("bg-primari");
+        }
+    });
+
+
+     /*** Smooth Scroll ***/ 
+    $(function () {
+        $('.nav-link, .smoth-scroll').on('click', function (event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top - 0
+            }, 1000);
+            event.preventDefault();
+        });
+    });
+
+/*********** Heading Slider **********/
+
+    $('.heading-area-slids').owlCarousel({
+        items: 1,
+        loop: true,
+        nav: false,
+        doots: false,
+        autoplay: true,
+        animateOut: 'fadeOutRight',
+        animateIn: 'fadeIn'
+
+    });
+    //Wow Animation
+    new WOW().init();
+
+    /** heading title area ***/
+    var element = $('.typed');
+
+    $(function () {
+        element.typed({
+            strings: ["Web Development.", "Quality Assurance.", "Mobile Development"],
+            typeSpeed: 100,
+            loop: true,
+            autoplay: true,
+        });
+    });
+
+/*********** Portfolio Section**********/
+
+    lightbox.option({
+        'imageFadeDuration': 800,
+        'resizeDuration': 500,
+        'wrapAround': true
+    });
+
+    $('.portfolio-area').mixItUp();
+
+/*********** Testimonial Section **********/
+
+    $('.testimonials').owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        nav: true,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+        dots: false
+    });
+
+
+/*********** Contact Area **********/
+    var form = $('#contact-form');
+
+    var formMessages = $('.form-message');
+    $(form).submit(function (e) {
+        e.preventDefault();
+        var formData = $(form).serialize();
+        $.ajax({
+                type: 'POST',
+                url: $(form).attr('action'),
+                data: formData
+            })
+            .done(function (response) {
+                $(formMessages).removeClass('error');
+                $(formMessages).addClass('success');
+                $(formMessages).text(response);
+
+                $('#contact-form input,#contact-form textarea').val('');
+            })
+            .fail(function (data) {
+                $(formMessages).removeClass('success');
+                $(formMessages).addClass('error');
+
+                if (data.responseText !== '') {
+                    $(formMessages).text(data.responseText);
+                } else {
+                    $(formMessages).text('Oops! An error occured and your message could not be sent.');
+                }
+            });
+    });
+    
+    
+/*********** Preloader **********/
+    $(window).on('load', function () {
+        $('.preloader').fadeOut();
+    });
+
+/*********** Back to top button **********/
+
+ /* Easy on scroll event listener  */
+ const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+}
+
+let backtotop = document.getElementById('back-to-top')
+if (backtotop) {
+  const toggleBacktotop = () => {
+    if (window.scrollY > 100) {
+      backtotop.classList.add('active')
+    } else {
+      backtotop.classList.remove('active')
+    }
+  }
+  window.addEventListener('load', toggleBacktotop)
+  onscroll(document, toggleBacktotop)
+}
+
+}(jQuery));
+
+
